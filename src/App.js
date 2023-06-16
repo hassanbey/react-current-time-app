be3dc13b9c34f81ec1a32279b516b04b29eb2f0e
hasanbajrami23 <92326923+hasanbajrami23@users.noreply.github.com>
2023-06-16 19:51:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { MoonLoader } from "react-spinners";
 
-function App() {
+const TimeDisplay = () => {
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function fetchData() {
+    setLoading(true);
+    const response = await fetch("http://worldtimeapi.org/api/ip");
+    const data = await response.json();
+    const datetime = new Date(data.datetime);
+    setTime(datetime.toLocaleTimeString());
+    setDate(datetime.toLocaleDateString());
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        {!loading && (
+          <div>
+            <p>Time: {time}</p>
+            <p>Date: {date}</p>
+          </div>
+        )}
+        {loading && (
+          <div className="spinner">
+            <MoonLoader loading={loading} size={70} />
+          </div>
+        )}
+        <button className="btn" onClick={fetchData}>
+          Current Time
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TimeDisplay;
